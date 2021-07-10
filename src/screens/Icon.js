c5a81ba98a8d07013f3d44f366e2687b634c90fc
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-07-10 12:17:44 -0300

    feat/Icons

--- src/screens/Icon.js
+++ src/screens/Icon.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-alert */
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Header} from 'react-native-elements';
+import {StyleSheet, View, Text} from 'react-native';
+import {Header, Icon} from 'react-native-elements';
 
 export default ({navigation}) => {
   return (
@@ -13,6 +14,32 @@ export default ({navigation}) => {
           text: 'ICON',
         }}
       />
+
+      <Text style={styles.text}>Plain</Text>
+      <Icon name="rowing" />
+
+      <Text style={styles.text}>Colored Material</Text>
+      <Icon name="g-translate" type="material" color="#00aced" />
+
+      <Text style={styles.text}>Colored EvilIcon</Text>
+      <Icon name="sc-telegram" type="evilicon" color="#517fa4" />
+
+      <Text style={styles.text}>Reversed</Text>
+      <Icon
+        reverse
+        name="ios-american-football"
+        type="ionicon"
+        color="#517fa4"
+      />
+
+      <Text style={styles.text}>Raised</Text>
+      <Icon
+        raised
+        name="heartbeat"
+        type="font-awesome"
+        color="#f50"
+        onPress={() => alert('foi')}
+      />
     </View>
   );
 };
@@ -23,5 +50,9 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
     marginTop: 10,
+  },
+  text: {
+    fontSize: 20,
+    marginTop: 30,
   },
 });
