40784bc7014f9cd2b6ca22bac72d89b2be8d19bc
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-07-12 20:42:22 -0300

    feat/Pricing

--- src/screens/Pricing.js
+++ src/screens/Pricing.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-alert */
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Header} from 'react-native-elements';
+import {Header, PricingCard} from 'react-native-elements';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default ({navigation}) => {
   return (
-    <View style={styles.container}>
+    <>
       <Header
         leftComponent={{
           onPress: () => navigation.goBack(),
@@ -13,13 +15,41 @@ export default ({navigation}) => {
           text: 'PRICING',
         }}
       />
-    </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <PricingCard
+            color="#4f9deb"
+            title="Free"
+            price="$0"
+            info={['1 User', 'Basic Support', 'All Core Features']}
+            button={{title: 'GET STARTED', icon: 'flight-takeoff'}}
+            containerStyle={styles.pricing}
+            onButtonPress={() => alert('foi')}
+          />
+          <PricingCard
+            color="#a82dea"
+            title="Starter"
+            price="$50"
+            info={['1 User', 'Basic Support', 'All Features']}
+            button={{title: 'GET STARTED', icon: 'flight-takeoff'}}
+            containerStyle={styles.pricing}
+            onButtonPress={() => alert('foi')}
+          />
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  pricing: {
+    width: '90%',
+    height: 295,
     justifyContent: 'flex-start',
     alignItems: 'center',
   },
