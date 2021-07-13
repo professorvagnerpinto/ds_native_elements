7519366c433144053a981c01ccfe3b22e1eaa0be
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-07-13 15:11:24 -0300

    feat/Slider

--- src/screens/Slider.js
+++ src/screens/Slider.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Header} from 'react-native-elements';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {StyleSheet, View, Animated, Text} from 'react-native';
+import {Header, Slider} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default ({navigation}) => {
+  const [value1, setValue1] = useState(0.0);
+  const [value2, setValue2] = useState(0.0);
+  const [value3, setValue3] = useState(0.0);
+  const [value4, setValue4] = useState(0.0);
+
   return (
     <View style={styles.container}>
       <Header
@@ -13,6 +20,56 @@ export default ({navigation}) => {
           text: 'SLIDER',
         }}
       />
+      <View style={styles.slider}>
+        <Text style={styles.text}>Default</Text>
+        <Slider value={value1} onValueChange={value => setValue1(value)} />
+        <Text>Value: {value1}</Text>
+      </View>
+      <View style={styles.slider}>
+        <Text style={styles.text}>Modified Scale Output</Text>
+        <Slider value={value2} onValueChange={value => setValue2(value)} />
+        <Text>Value: {value2 * 10}</Text>
+      </View>
+      <View style={styles.slider}>
+        <Text style={styles.text}>Replace Thumb with custom image</Text>
+        <Slider
+          value={value3}
+          onValueChange={value => setValue3(value)}
+          thumbStyle={{height: 40, width: 40, backgroundColor: 'transparent'}}
+          thumbProps={{
+            Component: Animated.Image,
+            source: {
+              uri: 'https://reactnativeelements.com/img/avatar/avatar--photo.jpg',
+            },
+          }}
+        />
+        <Text>Value: {value3 * 100}</Text>
+      </View>
+      <View style={styles.slider}>
+        <Text style={styles.text}>Set Custom Children inside thumb</Text>
+        <Slider
+          value={value4}
+          onValueChange={value => setValue4(value)}
+          maximumValue={140}
+          minimumValue={40}
+          step={1}
+          trackStyle={{height: 2, backgroundColor: 'transparent'}}
+          thumbStyle={{height: 40, width: 40, backgroundColor: 'transparent'}}
+          thumbProps={{
+            children: (
+              <Icon
+                name="heartbeat"
+                type="font-awesome"
+                size={40}
+                reverse
+                containerStyle={{bottom: 40, right: 40}}
+                color="#f50"
+              />
+            ),
+          }}
+        />
+        <Text>Value: {value4}</Text>
+      </View>
     </View>
   );
 };
@@ -22,5 +79,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
+  },
+  slider: {
+    width: '100%',
+    marginTop: 10,
+  },
+  text: {
+    fontSize: 20,
+    marginTop: 10,
+    textAlign: 'center',
   },
 });
