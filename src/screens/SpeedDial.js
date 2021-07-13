956e065f52323e37e82f37e51214b9c421a1a98c
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-07-13 15:42:00 -0300

    feat/Speed Dial

--- src/screens/SpeedDial.js
+++ src/screens/SpeedDial.js
@@ -1,8 +1,11 @@
-import React from 'react';
+/* eslint-disable no-alert */
+import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Header} from 'react-native-elements';
+import {Header, SpeedDial} from 'react-native-elements';
 
 export default ({navigation}) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <View style={styles.container}>
       <Header
@@ -13,6 +16,23 @@ export default ({navigation}) => {
           text: 'SPEED DIAL',
         }}
       />
+      <SpeedDial
+        isOpen={open}
+        icon={{name: 'edit', color: '#fff'}}
+        openIcon={{name: 'close', color: '#fff'}}
+        onOpen={() => setOpen(!open)}
+        onClose={() => setOpen(!open)}>
+        <SpeedDial.Action
+          icon={{name: 'add', color: '#fff'}}
+          title="Add"
+          onPress={() => alert('Add Something')}
+        />
+        <SpeedDial.Action
+          icon={{name: 'delete', color: '#fff'}}
+          title="Delete"
+          onPress={() => alert('Delete Something')}
+        />
+      </SpeedDial>
     </View>
   );
 };
