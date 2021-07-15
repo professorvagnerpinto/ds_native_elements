7fa1b363ea0fc13c5b516c4f338009808b9b9868
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-07-15 15:27:00 -0300

    feat/Circular Slider

--- src/screens/CircularSlider.js
+++ src/screens/CircularSlider.js
@@ -1,12 +1,23 @@
 import React, {useState} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {Header} from 'react-native-elements';
-//import {CircularSlider} from 'react-native-elements-universe';
+import {CircularSlider} from 'react-native-elements-universe';
+import {ScrollView} from 'react-native-gesture-handler';
+
+/*
+  Nota Técnica: instale essa dependência separadamente, execute o comando:
+  yarn add https://github.com/react-native-elements/react-native-elements-universe#dist
+  e yarn add react-native-svg (pois ela tem essa peerDependencies)
+  A versão que está no NPM vem vazia, sem o CircularSlider.
+*/
 
 export default ({navigation}) => {
-  const [value, setValue] = useState(0);
+  const [value1, setValue1] = useState(15);
+  const [value2, setValue2] = useState(30);
+  const [value3, setValue3] = useState(90);
+
   return (
-    <View style={styles.container}>
+    <>
       <Header
         leftComponent={{
           onPress: () => navigation.goBack(),
@@ -15,13 +26,36 @@ export default ({navigation}) => {
           text: 'CIRCULAR SLIDER',
         }}
       />
-      <Text>
-        Esta lib está com um bug na instalação, veja na pasta node_modules
-      </Text>
-      {/* <CircularSlider value={value} onChange={setValue} />
-      <CircularSlider value={value} noThumb />
-      <CircularSlider maxAngle={90} /> */}
-    </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text>
+            Esta lib requer uma inslação a parte. Veja o comentário no cebeçalho
+            do arquivo.
+          </Text>
+          <CircularSlider
+            value={value1}
+            onChange={setValue1}
+            showThumbText={true}
+            trackColor="green"
+          />
+          <CircularSlider
+            value={value2}
+            onChange={setValue2}
+            showThumbText={true}
+            thumbColor="red"
+            showText
+          />
+          <CircularSlider
+            value={value3}
+            onChange={setValue3}
+            maxAngle={90}
+            noThumb
+            showText
+            trackColor="orange"
+          />
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
